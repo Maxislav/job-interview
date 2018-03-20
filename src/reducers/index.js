2c3fceb79572f6e5106d9c42811fa3107241545c
Maxim Lipatov <lmaxim@mail.ru>
2018-03-20 20:12:42 +0200

    ini

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,8 +5,6 @@ export  function counterReducer(state = 0, action) {
 
     case 'INCREMENT':
       return state + 1
-    case 'INCREMENT_ASYNC':
-      return state + 1
     case 'INCREMENT_IF_ODD':
       return (state % 2 !== 0) ? state + 1 : state
     case 'DECREMENT':
