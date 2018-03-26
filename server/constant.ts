e52f50ad6fcb5abe4e2cdd8ae97733677478fe2e
Maxim Lipatov <lmaxim@mail.ru>
2018-03-26 13:30:54 +0300

    table debug

--- server/constant.ts
+++ server/constant.ts
@@ -1,5 +1,5 @@
 export const socketPort: number = 8888;
-export const tableListCont: number = 10;
+export const tableListCont: number = 100;
 export const timeUpadte = {
     min: 200,
     max: 2000
